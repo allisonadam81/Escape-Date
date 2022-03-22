382ec34a80682e775e4aa28a0d11a4ec5b976b54
Adam Allison <allisonadam81@gmail.com>
2022-03-21 20:18:00 -0700

    committing before pull

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'; 
 import { composeWithDevTools } from 'redux-devtools-extension';
-import reducers from './reducers/combinedReducers.js'
-import thunk from 'redux-thunk'; 
+import reducers from './reducers/combinedReducers.js';
+import thunk from 'redux-thunk';
 
 const store = createStore( reducers, composeWithDevTools(applyMiddleware(thunk)) );
 
